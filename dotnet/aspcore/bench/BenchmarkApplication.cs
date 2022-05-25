a198099160c1a93d60c36037e5153624b991bcd2
Tomáš Chaloupka <chalucha@gmail.com>
2022-05-25 20:53:20 +0200

    Update .Net to 6.0

--- dotnet/aspcore/bench/BenchmarkApplication.cs
+++ dotnet/aspcore/bench/BenchmarkApplication.cs
@@ -5,112 +5,93 @@ using System;
 using System.Buffers.Text;
 using System.IO.Pipelines;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 
-namespace PlatformBenchmarks
+namespace PlatformBenchmarks;
+
+public partial class BenchmarkApplication
 {
-    public partial class BenchmarkApplication
+    private readonly static AsciiString _applicationName = "Kestrel Platform-Level Application";
+    public static AsciiString ApplicationName => _applicationName;
+
+    private readonly static AsciiString _crlf = "\r\n";
+    private readonly static AsciiString _eoh = "\r\n\r\n"; // End Of Headers
+    private readonly static AsciiString _http11OK = "HTTP/1.1 200 OK\r\n";
+    private readonly static AsciiString _http11NotFound = "HTTP/1.1 404 Not Found\r\n";
+    private readonly static AsciiString _headerServer = "Server: K";
+    private readonly static AsciiString _headerContentLength = "Content-Length: ";
+    private readonly static AsciiString _headerContentLengthZero = "Content-Length: 0";
+    private readonly static AsciiString _headerContentTypeText = "Content-Type: text/plain";
+    private readonly static AsciiString _headerContentTypeJson = "Content-Type: application/json";
+    private readonly static AsciiString _headerContentTypeHtml = "Content-Type: text/html; charset=UTF-8";
+
+    private readonly static AsciiString _padHeader = "X-Test: 01234567890123456789012345012345678901234567890123456789";
+
+    private readonly static AsciiString _plainTextBody = "Hello, World!";
+
+    private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions();
+    private readonly static AsciiString _contentLengthGap = new string(' ', 4);
+
+    public static class Paths
     {
-        private readonly static AsciiString _applicationName = "Kestrel Platform-Level Application";
-        public static AsciiString ApplicationName => _applicationName;
+        public readonly static AsciiString Plaintext = "/";
+    }
 
-        private readonly static AsciiString _crlf = "\r\n";
-        private readonly static AsciiString _eoh = "\r\n\r\n"; // End Of Headers
-        private readonly static AsciiString _http11OK = "HTTP/1.1 200 OK\r\n";
-        private readonly static AsciiString _http11NotFound = "HTTP/1.1 404 Not Found\r\n";
-        private readonly static AsciiString _headerServer = "Server: K";
-        private readonly static AsciiString _headerContentLength = "Content-Length: ";
-        private readonly static AsciiString _headerContentLengthZero = "Content-Length: 0";
-        private readonly static AsciiString _headerContentTypeText = "Content-Type: text/plain";
-        private readonly static AsciiString _headerContentTypeJson = "Content-Type: application/json";
-        private readonly static AsciiString _headerContentTypeHtml = "Content-Type: text/html; charset=UTF-8";
+    private RequestType _requestType;
+    private int _queries;
 
-        private readonly static AsciiString _padHeader = "X-Test: 01234567890123456789012345012345678901234567890123456789";
+    public void OnStartLine(HttpVersionAndMethod versionAndMethod, TargetOffsetPathLength targetPath, Span<byte> startLine)
+    {
+        _requestType = versionAndMethod.Method == Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http.HttpMethod.Get ? GetRequestType(startLine.Slice(targetPath.Offset, targetPath.Length), ref _queries) : RequestType.NotRecognized;
+    }
 
-        private readonly static AsciiString _plainTextBody = "Hello, World!";
-
-        private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions();
-        private readonly static AsciiString _contentLengthGap = new string(' ', 4);
-
-        [ThreadStatic]
-        private static Utf8JsonWriter t_writer;
-
-        public static class Paths
+    private RequestType GetRequestType(ReadOnlySpan<byte> path, ref int queries)
+    {
+        if (path.SequenceEqual(Paths.Plaintext))
         {
-            public readonly static AsciiString Json = "/json";
-            public readonly static AsciiString Plaintext = "/";
+            return RequestType.PlainText;
         }
+        return RequestType.NotRecognized;
+    }
 
-        private RequestType _requestType;
-        private int _queries;
 
-#if NETCOREAPP5_0 || NET5_0
-        public void OnStartLine(HttpVersionAndMethod versionAndMethod, TargetOffsetPathLength targetPath, Span<byte> startLine)
+    private void ProcessRequest(ref BufferWriter<WriterAdapter> writer)
+    {
+        if (_requestType == RequestType.PlainText)
         {
-            _requestType = versionAndMethod.Method == HttpMethod.Get ? GetRequestType(startLine.Slice(targetPath.Offset, targetPath.Length), ref _queries) : RequestType.NotRecognized;
+            PlainText(ref writer);
         }
-#else
-        public void OnStartLine(HttpMethod method, HttpVersion version, Span<byte> target, Span<byte> path, Span<byte> query, Span<byte> customMethod, bool pathEncoded)
+        else
         {
-            _requestType = method == HttpMethod.Get ? GetRequestType(path, ref _queries) : RequestType.NotRecognized;
+            Default(ref writer);
         }
-#endif
+    }
+    private readonly static AsciiString _defaultPreamble =
+        _http11NotFound +
+        _headerServer + _crlf +
+        _headerContentTypeText + _crlf +
+        _headerContentLengthZero;
 
-        private RequestType GetRequestType(ReadOnlySpan<byte> path, ref int queries)
-        {
-            if (path.SequenceEqual(Paths.Plaintext))
-            {
-                return RequestType.PlainText;
-            }
-            else if (path.SequenceEqual(Paths.Json))
-            {
-                return RequestType.Json;
-            }
-            return RequestType.NotRecognized;
-        }
+    private static void Default(ref BufferWriter<WriterAdapter> writer)
+    {
+        writer.Write(_defaultPreamble);
 
+        // Date header
+        writer.Write(DateHeader.HeaderBytes);
+    }
 
-        private void ProcessRequest(ref BufferWriter<WriterAdapter> writer)
-        {
-            if (_requestType == RequestType.PlainText)
-            {
-                PlainText(ref writer);
-            }
-            else if (_requestType == RequestType.Json)
-            {
-                Json(ref writer, Writer);
-            }
-            else
-            {
-                Default(ref writer);
-            }
-        }
-        private readonly static AsciiString _defaultPreamble =
-            _http11NotFound +
-            _headerServer + _crlf +
-            _headerContentTypeText + _crlf +
-            _headerContentLengthZero;
-
-        private static void Default(ref BufferWriter<WriterAdapter> writer)
-        {
-            writer.Write(_defaultPreamble);
-
-            // Date header
-            writer.Write(DateHeader.HeaderBytes);
-        }
-
-        private enum RequestType
-        {
-            NotRecognized,
-            PlainText,
-            Json,
-            Fortunes,
-            SingleQuery,
-            Caching,
-            Updates,
-            MultipleQueries
-        }
+    private enum RequestType
+    {
+        NotRecognized,
+        PlainText,
+        Json,
+        Fortunes,
+        SingleQuery,
+        Caching,
+        Updates,
+        MultipleQueries
     }
 }
--- dotnet/aspcore/bench/Program.cs
+++ dotnet/aspcore/bench/Program.cs
@@ -1,56 +1,50 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Net;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using System.Runtime.InteropServices;
 
-namespace PlatformBenchmarks
+namespace PlatformBenchmarks;
+
+public class Program
 {
-    public class Program
+    public static string[] Args;
+
+    public static async Task Main(string[] args)
     {
-        public static string[] Args;
+        Args = args;
 
-        public static async Task Main(string[] args)
-        {
-            Args = args;
+        Console.WriteLine(BenchmarkApplication.ApplicationName);
+        Console.WriteLine(BenchmarkApplication.Paths.Plaintext);
+        DateHeader.SyncDateTimer();
 
-            Console.WriteLine(BenchmarkApplication.ApplicationName);
-            Console.WriteLine(BenchmarkApplication.Paths.Plaintext);
-            Console.WriteLine(BenchmarkApplication.Paths.Json);
-            DateHeader.SyncDateTimer();
+        var host = BuildWebHost(args);
+        var config = (IConfiguration)host.Services.GetService(typeof(IConfiguration));
+        await host.RunAsync();
+    }
 
-            var host = BuildWebHost(args);
-            var config = (IConfiguration)host.Services.GetService(typeof(IConfiguration));
-            await host.RunAsync();
-        }
+    public static IWebHost BuildWebHost(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+            .AddCommandLine(args)
+            .Build();
 
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables()
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .AddCommandLine(args)
-                .Build();
+        var host = new WebHostBuilder()
+            .UseBenchmarksConfiguration(config)
+            .UseKestrel((context, options) =>
+            {
+                var endPoint = context.Configuration.CreateIPEndPoint();
 
-            var host = new WebHostBuilder()
-                .UseBenchmarksConfiguration(config)
-                .UseKestrel((context, options) =>
+                options.Listen(endPoint, builder =>
                 {
-                    var endPoint = context.Configuration.CreateIPEndPoint();
+                    builder.UseHttpApplication<BenchmarkApplication>();
+                });
+            })
+            .UseStartup<Startup>()
+            .Build();
 
-                    options.Listen(endPoint, builder =>
-                    {
-                        builder.UseHttpApplication<BenchmarkApplication>();
-                    });
-                })
-                .UseStartup<Startup>()
-                .Build();
-
-            return host;
-        }
+        return host;
     }
 }
